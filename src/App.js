fed6368e78061dc29687a0475ef4be0f32e49654
Radisol <radisolfigueiredo@gmail.com>
2019-08-23 16:46:00 -0300

    card-produtos parcial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import Products from './components/cart/products/products'
+import Products from './components/Products/products'
+import Header from './components/Header/header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Products />
       </div>
     );
